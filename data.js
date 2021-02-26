55cb232272806bda23fc3f937b2ae475a5320324
Ernesto2909 <64288541+Ernesto2909@users.noreply.github.com>
2021-02-25 18:03:26 -0600

    Add files via upload

---
+++ data.js
@@ -0,0 +1,36 @@
+export default {
+    products:[{
+        _id:'1',
+        nombre: 'Camisa Slim',
+        categoria:'Camisa hombre',
+        imagen:'/images/c1.jpg',
+        precio:20,
+        marca:'Chaps',
+        raiting:4.5,
+        numReviews:10
+
+    },
+
+    {
+        _id:'2',
+        nombre: 'Camisa mujer',
+        categoria:'camisa mujer',
+        imagen:'/images/f2.jpg',
+        precio:12,
+        marca:'Dior',
+        raiting:4.0,
+        numReviews:10
+    },
+
+    {
+        _id:'3',
+        nombre: 'Camisa niño',
+        categoria:'camisa de niño',
+        imagen:'/images/n1.jpg',
+        precio:8,
+        marca:'Polo Club',
+        raiting:4.0,
+        numReviews:10
+    },
+]
+}
